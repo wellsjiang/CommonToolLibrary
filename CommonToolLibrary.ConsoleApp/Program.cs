9087e6d204c802d795c2740a6bf3bc192804228b
wellsjiang <wellsjiangcn@outlook.com>
2017-04-11 16:41:57 +0800

    控制台测试

--- CommonToolLibrary.ConsoleApp/Program.cs
+++ CommonToolLibrary.ConsoleApp/Program.cs
@@ -2,11 +2,14 @@
 
 namespace CommonToolLibrary.ConsoleApp
 {
+    using CommonToolLibrary.Extension.Converter;
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!".ToBase64());
+            Console.ReadKey();
         }
     }
 }
