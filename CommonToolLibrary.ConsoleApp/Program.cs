86b382383c7f9676e392a9cfc3fec5f1b5364fd2
wellsjiang <wellsjiangcn@outlook.com>
2017-04-12 18:17:07 +0800

    日志异步

--- CommonToolLibrary.ConsoleApp/Program.cs
+++ CommonToolLibrary.ConsoleApp/Program.cs
@@ -2,14 +2,38 @@
 
 namespace CommonToolLibrary.ConsoleApp
 {
+    using System.Threading.Tasks;
+
+    using CommonToolLibrary.Common.Model;
     using CommonToolLibrary.Extension.Converter;
+    using CommonToolLibrary.Extension.Log;
 
     class Program
     {
         static void Main(string[] args)
         {
+            //AsyncMethod();
+            LogService.Instance.Log(new LogRecordEntity(),true);
             Console.WriteLine("Hello World!".ToBase64());
             Console.ReadKey();
+        }
+
+
+        static async void AsyncMethod()
+        {
+            Console.WriteLine("Async Begin");
+            var result = await MyMethod();
+            Console.WriteLine("Async End");
+        }
+
+        static async Task<int> MyMethod()
+        {
+            var ss = "21325465756";
+            for (var index = 0; index < 10000000; index++)
+            {
+                ss.ToInt();
+            }
+            return 0;
         }
     }
 }
